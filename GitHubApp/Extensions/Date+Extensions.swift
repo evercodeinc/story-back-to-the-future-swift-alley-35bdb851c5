333d29f1e679fc233a67fd8cbbf68213661b9594
All3yp <noyallepereira@gmail.com>
2023-02-16 17:23:33 -0300

    Feat: Refactored travelToFuture() method to support since current date from year on the future

--- GitHubApp/Extensions/Date+Extensions.swift
+++ GitHubApp/Extensions/Date+Extensions.swift
@@ -14,8 +14,16 @@ extension Date {
   }
     
     func travelToFuture(years: Int) -> String? {
-     
-        // TODO
-        return nil
+      let calendar = Calendar.current
+      let dateComponents = calendar.dateComponents([.year, .month, .day], from: self)
+      guard let year = dateComponents.year,
+              let month = dateComponents.month,
+              let day = dateComponents.day else { return nil }
+      let newDateComponents = DateComponents(year: year + years, month: month, day: day)
+      guard let newDate = calendar.date(from: newDateComponents) else { return nil }
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "dd/MM/yyyy"
+      let dateFormatted = dateFormatter.string(from: newDate)
+      return dateFormatted
     }
 }
