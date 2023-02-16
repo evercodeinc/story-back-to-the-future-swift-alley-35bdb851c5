e2fe22b30652530cb8e98f0a038084e416131461
All3yp <noyallepereira@gmail.com>
2023-02-16 16:54:34 -0300

    Feat: Refactoring format() method to support differents date formaters.

--- GitHubApp/Extensions/Date+Extensions.swift
+++ GitHubApp/Extensions/Date+Extensions.swift
@@ -2,11 +2,16 @@ import Foundation
 
 extension Date {
     
-    static func format(dateString: String) -> String? {
-        
-        // TODO
-        return nil
-    }
+  static func format(dateString: String) -> String? {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+      guard let date = dateFormatter.date(from: dateString) else {
+          return nil
+      }
+      dateFormatter.dateFormat = "dd/MM/yyyy"
+      let dateFormatted = dateFormatter.string(from: date)
+      return dateFormatted
+  }
     
     func travelToFuture(years: Int) -> String? {
      
